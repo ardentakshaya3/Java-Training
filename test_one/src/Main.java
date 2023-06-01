18c66e8d9c7425e60d124fb1deb698d1358846e6
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-05-31 23:58:11 -0500

    Adding Student class and updating Main class

--- test_one/src/Main.java
+++ test_one/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Student s = new Student();
+        s.setName("Akshaya");
+        System.out.println("Student's name is "+s.getName());
     }
 }
