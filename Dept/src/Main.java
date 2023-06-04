3ca8e7465d649aadc7c752e9a6c0a7480ad6b4f4
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-03 19:11:00 -0500

    Updating project structure

--- Dept/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-       Name n = new Name();
-       System.out.println(n.getDepartmentSize());
-    }
-}
--- test_one/src/Main.java
+++ test_one/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -5,5 +7,37 @@ public class Main {
         Student s = new Student();
         s.setName("Akshaya");
         System.out.println("Student's name is "+s.getName());
+
+        System.out.println("Product Class");
+        //Product class
+        Scanner sc = new Scanner(System.in);
+        Product p = new Product();
+        Double resultI, resultD;
+        System.out.println("Enter price");
+        Double price = sc.nextDouble();
+        p.setPrice(price);
+        resultI = p.getPrice(4);
+        resultD = p.getPrice(5.8);
+        System.out.println("Price with 4 is "+resultI);
+        System.out.println("Price with 5.8 is "+resultD);
+
+        System.out.println("RUn-time Poly");
+        //Run-time polymorphism (Planet.java)
+        Planet c= new Planet();
+        Planet c1 = new First();
+        Planet c2 = new Second();
+        Planet c3 = new Third();
+        c.printPlanet();
+        c1.printPlanet();
+        c2.printPlanet();
+        c3.printPlanet();
+
+        System.out.println("Department class");
+        //Department assignment
+        Name n = new Name();
+        System.out.println(n.getDepartmentSize());
+
+
+
     }
 }
