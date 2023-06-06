de793ba129e474a3899a73b536f6b7003d59ad0b
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-06 11:06:51 -0500

    Adding Person file

---
+++ week2/src/PersonType.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PersonType {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter age of person");
+        int age=sc.nextInt();
+        if(age<13)
+            System.out.println("Person is kid");
+        else if (age>=13 && age<=19)
+            System.out.println("Person is teen");
+        else
+            System.out.println("Person is adult");
+    }
+}
