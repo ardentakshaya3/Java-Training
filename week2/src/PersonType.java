5db3ef8bd64eba67e774c3747d81459df4f50f34
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-06 16:00:37 -0500

    Updating Person file

--- week2/src/PersonType.java
+++ week2/src/PersonType.java
@@ -1,14 +1,18 @@
 import java.util.*;
 public class PersonType {
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter age of person");
-        int age=sc.nextInt();
-        if(age<13)
-            System.out.println("Person is kid");
-        else if (age>=13 && age<=19)
-            System.out.println("Person is teen");
+        int age = sc.nextInt();
+        if (age > 0) {
+            if (age < 13)
+                System.out.println("Person is kid");
+            else if (age >= 13 && age <= 19)
+                System.out.println("Person is teen");
+            else
+                System.out.println("Person is adult");
+        }
         else
-            System.out.println("Person is adult");
+            System.out.println("Invalid age");
     }
 }
