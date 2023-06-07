b54119cacd28b3f2f446c09cf99cb3358fd2de6d
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-07 10:45:22 -0500

    Adding FindArray file

---
+++ week2/src/FindArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class FindArray
+{
+    public static void main(String args[])
+    {
+        int l,s,i;
+        int a[] = new int[]{8, 2, 3, 4, 5, 10, 9, 3, 6};
+        int n = a.length;
+        l=s=a[0];
+        for(i=1;i<n;i++)
+        {
+            if(a[i]>l)
+                l=a[i];
+
+            if(a[i]<s)
+                s=a[i];
+        }
+        System.out.println("The smallest element is " + s );
+        System.out.print("The largest element is " + l );
+    }
+}
