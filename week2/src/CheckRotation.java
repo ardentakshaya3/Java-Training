a95ff283a5159e4b5475c2ef305bb2da784471e7
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-08 22:10:59 -0500

    Adding String Rotation file

---
+++ week2/src/CheckRotation.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CheckRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String 1");
+        String s1 = sc.nextLine();
+        System.out.println("Enter String 2");
+        String s2 = sc.nextLine();
+        String s3 = "";
+        if (s1.length() != s2.length()) {
+            System.out.println("False");
+        }
+        else {
+            s3 = s1 + s1;
+            if (s3.contains(s1)) {
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+        }
+    }
+}
