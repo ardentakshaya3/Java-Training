a28033a69dcabc1f036d1c920e494bfe9f4fcd4a
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-13 14:13:53 -0500

    Updating CustomException files

--- week3/src/CustomException.java
+++ week3/src/CustomException.java
@@ -1,18 +1,45 @@
 import java.lang.Exception;
+import java.util.Scanner;
 
-class CustomMessage extends Exception {
-    public CustomMessage(String a) {
-        super(a);
+class SameName extends Exception
+{
+    public SameName(String message)
+    {
+        super(message);
     }
 }
 
+class CustomMessage {
+    String fn, ln;
+    public CustomMessage(String a, String b) {
+        this.fn = a;
+        this.ln=b;
+    }
+    public String check() throws SameName{
+//        System.out.println(this.fn.equals(this.ln));
+        if(this.fn.equals(this.ln))
+            throw new SameName("First name and last name are similar. Enter again!ss");
+        else
+            return fn+" "+ln;
+    }
+
+}
+
 public class CustomException {
-    public static void main(String args[]) throws CustomMessage {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter first name");
+        String fn = s.nextLine();
+        System.out.println("Enter last name");
+        String ln = s.nextLine();
+        CustomMessage cm = new CustomMessage(fn,ln);
         try {
-            System.out.println(2/0);
+            String st = cm.check();
+            System.out.println(st);
         }
-        catch (Exception e) {
-            throw new CustomMessage("Arithmetic Exception: Number not divisible by zero!");
+        catch (SameName e) {
+            e.printStackTrace();
         }
+
     }
 }
