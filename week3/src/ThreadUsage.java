ac002dee1f222897b6f6c098526f88bd671512ab
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-14 15:56:22 -0500

    Updating Thread Usage files

--- week3/src/ThreadUsage.java
+++ week3/src/ThreadUsage.java
@@ -1,6 +1,6 @@
 import java.lang.Thread;
 
-class NumberThread extends Thread {
+class NThread extends Thread {
     public void run() {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
@@ -10,7 +10,7 @@ class NumberThread extends Thread {
 
 public class ThreadUsage {
     public static void main(String[] args) {
-        NumberThread thread = new NumberThread();
+        NThread thread = new NThread();
         thread.start();
     }
 }
