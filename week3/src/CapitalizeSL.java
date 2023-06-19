8eda140e52ff05261b7471c882134cfb156f1e22
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-19 14:57:03 -0500

    updating file

--- week3/src/CapitalizeSL.java
+++ week3/src/CapitalizeSL.java
@@ -3,13 +3,10 @@ import java.util.Arrays;
 public class CapitalizeSL {
     public static void main(String[] args) {
         String[] sports = {"badminton", "football", "basketball", "swimming", "golf"};
-        capitalizeAndSort(sports);
+        Arrays.sort(sports);
         Arrays.stream(sports)
                 .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1))
                 .forEach(System.out::println);
     }
 
-    public static void capitalizeAndSort(String[] sports) {
-        Arrays.sort(sports);
-    }
 }
