4f101b636a9b8b9cb72570e1e3d46dd15f1272fd
srpakshaya <AkshayaSatarajupalli@my.unt.edu>
2023-06-21 13:10:27 -0500

    updating LetterCount file

--- week4/LetterCount.java
+++ week4/LetterCount.java
@@ -6,19 +6,18 @@ public class LetterCount {
         Map<Character, Integer> charCount = new HashMap<>();
 
         for (char c : str.toCharArray()) {
-            if (charCount.containsKey(c)) {
-                charCount.put(c, charCount.get(c) + 1);
-            } else {
-                charCount.put(c, 1);
+            if(c!=' ') {
+                if (charCount.containsKey(c)) {
+                    charCount.put(c, charCount.get(c) + 1);
+                } else {
+                    charCount.put(c, 1);
+                }
             }
+
         }
         System.out.println();
-
         charCount.forEach((key, value) -> System.out.println(key + ":" + value));
 
-//        for (entry in charCount) {
-//            System.out.printf("%c-%d, ", entry.getKey(), entry.getValue());
-//        }
     }
 
     public static void main(String[] args) {
